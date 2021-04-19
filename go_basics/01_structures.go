5c25e419470b99f5005c0937517109b527019f30
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-04-19 16:26:19 -0700

    structures go

---
+++ go_basics/01_structures.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// func printAge(ages ...int) {
+// 	for _, value := range ages {
+// 		fmt.Println(value)
+// 	}
+// }
+
+func average(numbers ...float64) float64 {
+	total := 0.0
+	for _, number := range numbers {
+		total += number
+	}
+	return total / float64(len(numbers))
+}
+
+func main() {
+	fmt.Println(average(1.2, 3.4, 10.5))
+}
