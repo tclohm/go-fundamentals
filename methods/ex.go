76715b80b395d76f4f58f2b55fd2272b93838fc6
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-04-20 11:52:19 -0700

    stated methods

---
+++ methods/ex.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+type User struct {
+	ID	int
+	Firstname, Lastname, Email string
+}
+
+// Group is a set of users
+type Group struct {
+	role string
+	users []User
+	newestUser User
+	spaceAvailable bool
+}
+
+func (u *User) describe() string {
+	desc := fmt.Sprintf("Name: %s %s, Email: %s", u.Firstname, u.Lastname, u.Email)
+	return desc
+}
+
+func main() {
+	user := User{ID: 1, Firstname: "Yaya", Lastname: "Ding Dong", Email: "yaya@dingdong.edu"}
+
+	desc := user.describe()
+
+	fmt.Println(desc)
+}
