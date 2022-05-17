a1b245b13e56befb2dd5094102a8f4977367602a
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-05-17 16:12:54 -0700

    packages and installing

---
+++ packages/main/main.go
@@ -0,0 +1,11 @@
+package main // launches program
+
+import (
+	"common/msg"
+	"common/display"
+)
+
+func main() {
+	msg.Hi()
+	display.Display("hello from display")
+}
---
+++ packages/msg/msg.go
@@ -0,0 +1,7 @@
+package msg
+
+import "common/display"
+
+func Hi() {
+	display.Display("hi")
+}
