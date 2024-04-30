e34931619476799213cdfbc9cba4307c94d166b5
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-04-30 14:23:21 -0700

    cool beans

--- channels/channels_vs_wait_groups.go
+++ channels/channels_vs_wait_groups.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"time"
 	"sync"
+	"math/rand"
 )
 
 func doWork(d time.Duration, wg *sync.WaitGroup) {
@@ -16,13 +17,28 @@ func doWork(d time.Duration, wg *sync.WaitGroup) {
 	wg.Done()
 }
 
+func tell(d time.Duration, resChan chan string) {
+	fmt.Println("Hey, I'm doing work....")
+	time.Sleep(d)
+	resChan <- fmt.Sprintf("I'm done with job ID: %v", rand.Intn(100))
+}
+
 
 func main() {
 	start := time.Now()
-	wg := sync.WaitGroup{}
-	wg.Add(2)
-	go doWork(time.Second * 2, &wg)
-	go doWork(time.Second * 4, &wg)
-	wg.Wait()
+	//wg := sync.WaitGroup{}
+	//wg.Add(2)
+	//go doWork(time.Second * 2, &wg)
+	//go doWork(time.Second * 4, &wg)
+	//wg.Wait()
+
+	resultChan := make(chan string)
+	go tell(time.Second * 2, resultChan)
+	go tell(time.Second * 4, resultChan)
+
+	res1 := <- resultChan
+	fmt.Println(res1)
+	res2 := <- resultChan
+	fmt.Println(res2)
 	fmt.Printf("work took %v seconds\n", time.Since(start))
 }
