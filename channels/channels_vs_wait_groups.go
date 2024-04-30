83f72709fe550f6e5558ab373522a309c34c74eb
Taylor Lohman <taylor.lohman.tl@gmail.com>
2024-04-30 14:08:17 -0700

    wait for the longest process

--- channels/channels_vs_wait_groups.go
+++ channels/channels_vs_wait_groups.go
@@ -6,17 +6,23 @@ package main
 import (
 	"fmt"
 	"time"
+	"sync"
 )
 
-func doWork(d time.Duration) {
+func doWork(d time.Duration, wg *sync.WaitGroup) {
 	fmt.Println("doing work....")
 	time.Sleep(d)
 	fmt.Println("done")
+	wg.Done()
 }
+
 
 func main() {
 	start := time.Now()
-	go doWork(time.Second * 2)
-	go doWork(time.Second * 4)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go doWork(time.Second * 2, &wg)
+	go doWork(time.Second * 4, &wg)
+	wg.Wait()
 	fmt.Printf("work took %v seconds\n", time.Since(start))
 }
